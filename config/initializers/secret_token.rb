a452a57bce325786f651b7c61339e9cd64e54440
mouse <linhongshimouse@gmail.com>
2012-03-17 15:57:27 +0800

    创建pf项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pf::Application.config.secret_token = 'bc8d72ba7e7c301f767c47f0e9236474377c4e5c45ddbd708c875c8aea02269b9ec5cd00871ebc39eb0818910ae779e58c86322e3889a93e67e4674184e7ec08'
