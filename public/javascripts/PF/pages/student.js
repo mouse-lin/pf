f718a67421222cf8cd5ca157f056b81f9bb65191
mouse <linhongshimouse@gmail.com>
2012-03-22 15:28:40 +0800

    去除最外层多出的border布局

--- public/javascripts/PF/pages/student.js
+++ public/javascripts/PF/pages/student.js
@@ -1,1 +1,25 @@
 Ext.ns("Pf.classes.student");
+var store_name  = new Ext.data.JsonStore({ 
+    fields: [
+      
+    ],
+    root: 'content',
+    url: 'store_url',
+    totalProperty:'total',
+    method:'GET',
+});
+var cm = new Ext.grid.ColumnModel([
+    new Ext.grid.RowNumberer(),
+    { header: '', sortable: true, dataIndex: '',editor:new Ext.form.TextField()},
+    { header: '', sortable: true, dataIndex: '',editor:new Ext.form.TextField()},
+]);
+var tbar = [
+  { iconCls: '', text: '', handler: function(){ },
+];
+var  grid_name   = new Ext.grid.EditorGridPanel({ 
+    viewConfig: { forceFit: true },
+    store: store_name ,
+    cm: cm,
+    tbar: tbar, 
+)};
+return  grid_name  ;
